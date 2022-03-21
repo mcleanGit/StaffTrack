c4135b4322e93a5f07359aca908d3516a271bd5b
Don McLean <mclean@utoronto.ca>
2022-03-20 22:03:54 -0400

    fix details

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,7 +17,7 @@ INSERT INTO roles (title, salary, department_id)
   ('Account Manager', 160000, 3),
   ('Acountant', 125000, 3),
   ('Legal Team Lead', 250000, 4),
-  ('Lawyer', 19000, 4);
+  ('Lawyer', 190000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id )
 VALUES

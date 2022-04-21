d464374771bdc34c6b9394c8fb3a981ee3d0bdd3
Don McLean <mclean@utoronto.ca>
2022-04-21 11:33:14 -0400

    updates generic password etc

--- db/connection.js
+++ db/connection.js
@@ -3,16 +3,15 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
  host: 'localhost',
- port: '3001',
+// port: '3001',
  user: 'root',
- password: '',
+ password: '02468a13579B!',
  database: 'employeeTracker_db'
 })
 
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connection made!");  // works to here
-  initialPrompt();
 });
 
 module.exports = connection;

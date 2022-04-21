20203d1c4418ce00acce665ba44e90450776be04
Don McLean <mclean@utoronto.ca>
2022-04-21 11:44:00 -0400

    adds this class for organizing queries

--- sqlQueries.js
+++ sqlQueries.js
@@ -9,13 +9,25 @@ class sqlQueries {
   }
 
   // ETC
-  /*
-  viewRoles
+  
+  viewRoles() {
+   return this.connection.promise().query("Select role.id, role.title, role.department, role.salary from employees;")
+  }
 
-  viewEmployees
+  viewEmployees() {
+   return this.connection.promise().query("Select employee.id employee.first_name, employee.last_name, employee.role, employee.department, employee.salary, employee.manager from employees;")
+  }
 
-  addDepartment
+  addDepartment() {
+   return this.connection.promise().query("Insert into departments set ?;")
+  }
  
-  addRole
+  addRole () {
+   return this.connection.promise().query("Insert into roles set ?;")
+  }
 
-  addEmployee
+  addEmployee () {
+   return this.connection.promis().query("Insert into employees set ?;")
+  }
+ 
+}

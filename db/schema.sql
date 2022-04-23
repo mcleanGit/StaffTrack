345ccfb1602a23cbd6a68b35c49fc634e239c971
Don McLean <mclean@utoronto.ca>
2022-04-23 15:10:09 -0400

    singularize table titles

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
  name VARCHAR(32) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
  title VARCHAR(32) UNIQUE NOT NULL,
  salary DECIMAL UNSIGNED NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE roles (
  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
  first_name VARCHAR(30) NOT NULL,
  last_name VARCHAR(30) NOT NULL,
